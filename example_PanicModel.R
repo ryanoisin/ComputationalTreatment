a5fd97112f50fb9e76675ebadd381dcb947fc227
ryanoisin <ryanoisin@gmail.com>
2023-07-31 17:23:06 +0200

    clean up preprocessing

---
+++ example_PanicModel.R
@@ -0,0 +1,138 @@
+# code to simulate example data from the PanicModel and plot it
+
+library(PanicModel)
+library(RColorBrewer)
+library(scales)
+
+source("symptom_functions.R")
+
+# --------------------------------------------------------------------------- #
+# --------------------------------- Figure 3 -------------------------------- #
+# --------------------------------------------------------------------------- #
+
+# ----- Specify Intervention -----
+# Here: CBT
+baseline_days <- 8
+baseline_weeks <- 0 # one week baseline
+# March 8th: This now matches the simulation
+
+tx <- list("I1" = baseline_days+baseline_weeks*7+c(1), # Psychoeducation -> AS
+           "I2" = baseline_days+baseline_weeks*7+c(1), # Psychoeducation -> ES
+           "I3" = (baseline_days+baseline_weeks+(1:2)*7)+c(1), # Cognitive restructuring -> AS
+           "I4" = baseline_days+c((baseline_weeks+1)*7+1:28), # Interoceptive Exposure -> A, E
+           "I5" = baseline_days+(baseline_weeks+3)*7+(1:14)[rep(c(TRUE,TRUE), 7)]) # In vivo exposure -> Context
+
+treatment_days <- max(unlist(lapply(tx, max)))
+
+
+# ----- Simulate from Panic Model -----
+
+# Fill in last values from above:
+last_row <- out_base$outmat[nrow(out_base$outmat), ]
+initial_specified <- list("S" = last_row$S,
+                          "A" = last_row$A,
+                          "PT" = last_row$PT,
+                          "E" = last_row$E,
+                          "X" = last_row$X)
+
+# Simulate
+total_days <- treatment_days + 2*7 # treatment + two-week follow up
+time <- total_days*60*24 # full treatment period
+n_weeks_T <- total_days/7
+
+set.seed(2)
+out_treat <- simPanic(time = 1:time,
+                      stepsize = .001,
+                      initial = initial_specified,
+                      tx = tx)
+results_treat <- out_treat$outmat
+
+# saveRDS(out_treat, file="Files/data_treat_1w_baseline_2w_followup.RDS")
+out_treat <- readRDS(file="Files/data_treat_1w_baseline_2w_followup.RDS")
+
+SimTreat <- out_treat$outmat
+
+# ----- Compute Symptoms per week -----
+week_length_T <- nrow(out_treat$outmat)/n_weeks_T
+l_symptoms_T <- list()
+for(i in 1:n_weeks_T) {
+  l_symptoms_T[[i]] <- getSymptoms(out_treat$outmat[(1+(i-1)*week_length_T):(week_length_T*i),])
+}
+
+m_symptoms_T <- do.call(rbind, l_symptoms_T)
+
+
+# ----- Make Figure -----
+
+display.brewer.all(n=NULL, type="all", select=NULL, exact.n=TRUE, colorblindFriendly=FALSE)
+
+# cols <- brewer.pal(5, "Dark2")
+cols <- c("grey", "black", "lightblue", "tomato", "orange", "#dd1c77")
+
+sc <- 0.8
+pdf("Figures/Fig_SimData_Treatment_July18_23.pdf", width = 7*sc, height = 7*sc)
+
+# Make Layout
+lmat <- matrix(1:3, nrow=3)
+lo <- layout(lmat, heights = c(.35, 0.8, .55))
+# layout.show(lo)
+
+# --- Panel: Treatment ---
+int_labels <- c("Psychoeduction",
+                "Cognitive Restructuring",
+                "Interoceptive Exposure",
+                "In Vivo Exposure")
+par(mar=c(0,4.5,0,2))
+plot.new()
+plot.window(xlim = c(0, total_days),  ylim=c(0, 1.2))
+segments(rep(-2, 4), v_trt_y, rep(total_days, 4), v_trt_y, col="black", lty=1)
+text(rep(-2, 4), v_trt_y+0.075, labels = int_labels, adj=0,  col="black")
+pch_intero <- abs(out_treat$adherence-1) + 20
+cex_intero <- pch_intero
+cex_intero[cex_intero==20] <- 1.5
+cex_intero[cex_intero==21] <- 1.1
+
+points(tx[[1+1]], rep(v_trt_y[1], length(tx[[1+1]])), pch=20, cex=1.5)
+points(tx[[2+1]], rep(v_trt_y[2], length(tx[[2+1]])), pch=20, cex=1.5)
+points(tx[[3+1]], rep(v_trt_y[3], length(tx[[3+1]])), pch=pch_intero, cex=cex_intero, bg="white")
+points(tx[[4+1]], rep(v_trt_y[4], length(tx[[4+1]])), pch=20, cex=1.5)
+
+# --- Panel: Panic Model Data ---
+# right data
+lwd <- 1
+par(mar=c(1,4.5,0,2))
+plot.new()
+plot.window(xlim = c(0, length(SimTreat$A)),  ylim=c(-0.6, 1.1))
+axis(2, las=2, seq(-0.5, 1, length=7))
+no_x_ticks <- 9
+
+lwd <- 1.5
+lines(SimTreat$A, col = cols[1], lwd = lwd)
+lines(SimTreat$PT, col = cols[2],lwd = lwd)
+lines(SimTreat$E, col = cols[3], lwd = lwd, lty=2)
+lines(SimTreat$X, col = cols[5],lwd = lwd)
+lines(SimTreat$S, col = cols[4],  lwd = lwd)
+
+# Legend
+rect(10260*6.34, 0.5, 10260*8, 1, col="white", border=FALSE)
+legend("topright", legend = c("Arousal","Perceived Threat", "Escape Behavior", "Arousal Schema", "Escape Schema"),
+       text.col = cols ,cex = 1.15, ncol=1
+       ,bty = "n"
+)
+
+# --- Panel: Symptoms ---
+# Rigt: symptom data
+par(mar=c(4,4.5,0,2))
+plot.new()
+plot.window(xlim=c(0, total_days), ylim=c(0,20))
+axis(1, 0:8, at=seq(0, total_days, length=9))
+axis(2, las=2)
+mtext("Weeks", side = 1, line = 2.5, cex = 0.8)
+lines(0:total_days, c(NA,NA,rep(m_symptoms_T[, 6], each=7)), col=cols[6], lwd=lwd)
+
+legend("topright", legend = c("Symptom Sum Score"),
+       text.col = cols[6], bty = "n",cex = 1.15, ncol=1)
+
+
+dev.off()
+
--- sim_preprocess.R
+++ sim_preprocess.R
@@ -9,14 +9,17 @@
 # ----------------- Output 1: outfiles.RDS --------------------------------
 # -------------------------------------------------------------------------
 library(abind)
+source("symptom_functions.R")
 
 ### Data Preparation Step 1: Import and Organize Data -----
 
-
 l_all <- list()
-dir <- "simcode/output/"
+dir <- "Simulation/output/"
 v_files <- list.files(dir)
 n_files <- length(v_files)
+
+symptoms <- list(baseline = list(), g1 = list(), g2 = list(),
+                 g3 = list(), g4 = list(), g5 = list())
 
 # each file contains ~16 "people"
 # for each person we have a multivariate time series for
@@ -26,11 +29,15 @@ n_files <- length(v_files)
 # we don't need to extract anything else
 
 IDcount <- 1
+# extract full information for two selected individuals
+idsel <- c(25,55,133,207,429,134,168)
+# first 5 are bt responders, last two are ct responders
 
 for(i in 1:n_files){
-  tmp <- readRDS(paste0(dir, v_files[i]))
+  tmp <- readRDS(paste0(dir, "IntSim6_Iter", i, ".RDS"))
 
   for(j in 1:length(tmp)){
+    if(IDcount < 501){
     print(paste("file", i, "person",IDcount))
     theter <- as.data.frame(tmp[[1]]$heter)
 
@@ -63,7 +70,6 @@ for(i in 1:n_files){
       adher5 <- tadher5
 
     }else{ # otherwise, bind output objects together
-    if(IDcount < 501){
       heter <- rbind(heter,theter)
       out_baseline <- rbind(out_baseline, tout_baseline)
       out_group1 <- rbind(out_group1, tout_group1)
@@ -75,90 +81,71 @@ for(i in 1:n_files){
       adher4 <- rbind(adher4, tadher4)
       adher5 <- rbind(adher5, tadher5)
     }
+
+  # Create Symptoms Output
+      for(q in 1:6){
+        dframe  <- tmp[[j]][[1+q]]$outmat
+        wind <- seq(1,nrow(dframe), 10080)
+        nw <- length(wind) -1 # number of weeks
+
+        weeks <- cbind(wind[1:nw], wind[2:(nw +1)])
+
+        outid <- cbind(t(apply(weeks,1,function(r){
+          getSymptoms(dframe[r[1]:r[2],])
+        })),IDcount)
+
+        colnames(outid)[1:11] <- c("q1_panic", "q2_padistress", "q3_fear", "q4_avoid","q5_context", "sumscore",
+                                   "n_panic","q2_distress_cont", "q3_fear_cont", "q4_avoid_cont", "q5_context_cont")
+
+          symptoms[[q]][[IDcount]] <- outid
+
+      } # end treatment arm loop
+
     }# end of if
+
+    # Additional: If selected participant ID, save all data
+    if(IDcount %in% idsel){
+      baseline <- round(tmp[[j]]$out_baseline$outmat,2)
+      ct <- round(tmp[[j]]$out_group2$outmat,2)
+      cbt <- round(tmp[[j]]$out_group4$outmat,2)
+      saveRDS(list(baseline = baseline, ct = ct, cbt = cbt),
+              file = paste0("Files/data_id",idsel[idsel == IDcount],".RDS")
+      )
+    } # end if sub selection
     IDcount <- IDcount + 1
   } # end of loop through list
   rm(tmp) # tidy -up (probably makes no difference except a slight slow down)
 }
 
-# 465
+
 n_id <- IDcount - 1
 
 # now merge some of these files together into arrays
 
 out_treat <- abind(out_group1,out_group2, out_group3, out_group4,out_group5, along = 3)
 adher <- list(NULL, NULL,adher3,adher4, adher5)
-heter
+
+# save files
 saveRDS(list(out_baseline = out_baseline,
              out_treat = out_treat,
              adher = adher,
              heter = heter),
-        file = "files/outfiles.RDS")
+        file = "Files/outfiles.RDS")
+
+# save symptom file
+
+# # move "last observation of baseline" to "first observation of treatment"
+# # this makes some later data processing easier, as we have a "week 0"
+# for(id in 1:500){
+#   for(G in 2:6){
+#     # last baseline measurement becomes "week 0" of treatment
+#     symptoms[[G]][[id]] <- rbind(symptoms$baseline[[id]][4,],
+#                                  symptoms[[G]][[id]])
+#   }
+#   # drop last week of baseline measurement from baseline period
+#   symptoms$baseline[[id]] <-  symptoms$baseline[[id]][-4,]
+# }
+
+saveRDS(symptoms, file = "Files/symptoms_out.RDS")
 
 rm(list=ls())
-
-# -------------------------------------------------------------------------
-# ----------------- Output 2: symptoms_out.RDS ----------------------------
-# -------------------------------------------------------------------------
-source("symptom_functions.R")
-library(PanicModel)
-
-l_all <- list()
-dir <- "simcode/output/"
-v_files <- list.files(dir)
-n_files <- length(v_files)
-
-# create storage
-symptoms <- list(baseline = list(), g1 = list(), g2 = list(), g3 = list(), g4 = list(), g5 = list())
-
-IDcount <- 1
-
-# Loop through files. Extract only the first 500 participants
-
-for(i in 1:n_files){
-  tmp <- readRDS(paste0(dir, v_files[i]))
-
-  for(j in 1:length(tmp)){
-    print(paste("file", i, "person",IDcount))
-
-    for(q in 1:6){
-      print(q)
-      dframe  <- tmp[[j]][[1+q]]$outmat
-      wind <- seq(1,nrow(dframe), 10080)
-      nw <- length(wind) -1 # number of weeks
-
-      weeks <- cbind(wind[1:nw], wind[2:(nw +1)])
-
-      outid <- cbind(t(apply(weeks,1,function(r){
-        getSymptoms(dframe[r[1]:r[2],])
-      })),IDcount)
-
-      colnames(outid)[1:11] <- c("q1_panic", "q2_padistress", "q3_fear", "q4_avoid","q5_context", "sumscore",
-                                 "n_panic","q2_distress_cont", "q3_fear_cont", "q4_avoid_cont", "q5_context_cont")
-
-      if(IDcount < 501){
-        symptoms[[q]][[IDcount]] <- outid
-      }
-    } # end treatment arm loop
-    IDcount <- IDcount +1
-  } # end person loop
-
-} #end file loop
-
-
-# move "last observation of baseline" to "first observation of treatment"
-# this makes some later data processing easier, as we have a "week 0"
-for(id in 1:500){
-  for(G in 2:6){
-    # last baseline measurement becomes "week 0" of treatment
-    symptoms[[G]][[id]] <- rbind(symptoms$baseline[[id]][4,],
-                                 symptoms[[G]][[id]])
-  }
-  # drop last week of baseline measurement from baseline period
-  symptoms$baseline[[id]] <-  symptoms$baseline[[id]][-4,]
-}
-
-saveRDS(symptoms, file = "files/symptoms_out.RDS")
-
-
-
